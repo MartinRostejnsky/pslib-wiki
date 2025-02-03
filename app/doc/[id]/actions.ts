17df6ba410d2f8c9d40b673ae0c8cb8a221b68ca
mapetr <info@mapetr.moe>
2025-02-03 21:22:40 +0100

    fix build errors

--- app/doc/[id]/actions.ts
+++ app/doc/[id]/actions.ts
@@ -1,7 +1,6 @@
 "use server"
 
 import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
-import {Editor} from "@tiptap/core";
 import {auth} from "@clerk/nextjs/server";
 import {uuidv4} from "lib0/random";
 import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
