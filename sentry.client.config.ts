9a0bbdbb13fdf134d79c8593360fe5ca87412748
mapetr <info@mapetr.moe>
2025-02-04 00:25:59 +0100

    add sentry

---
+++ sentry.client.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://40efb74d82e40b24a1d71adc87dbfcac@o4508695722786816.ingest.de.sentry.io/4508757847507024",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+  enabled: process.env.NODE_ENV !== "development"
+});
